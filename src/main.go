9b413909e81a3828dd8432c4d8690c2a18b28ff8
sromeo <romeo.sanchez@ynov.com>
2023-09-19 08:41:03 +0200

    main update with secret weapon

--- src/main.go
+++ src/main.go
@@ -21,40 +21,40 @@ type character struct {
 func Write(sentence string) {
 	for _, letter := range sentence {
 		os.Stdout.WriteString((string(letter)))
-		time.Sleep(100)
+		time.Sleep(1000)
 	}
 }
 
 var player character
 
 func main() {
-	Write("Hey you, yeah you, you're new in the guild right ? What's your name ? \n")
+	Write("Hey you, yeah you, you're new in the guild right ? What's your name ? \n\n")
 	name := ""
 	fmt.Scanln(&name)
 
 	player.name = name
 
-	Write("Ewh what a disgusting name, " + name + ", hum, do you want a beer to welcome you into the guild ?\n")
+	Write("Ewh what a disgusting name, " + name + ", hum, do you want a beer to welcome you into the guild ?\n\n")
 	Write("1 - yes \n2 - no \n")
 	answer1 := ""
 	fmt.Scanln(&answer1)
 
 	if answer1 == "1" {
-		Write("You start drinking but soon enough, you begin to stagger... the beer might have been poisonous... lol")
+		Write("You start drinking but soon enough, you begin to stagger... the beer might have been poisonous... lol \n")
 		player.health_point = 50
 	} else if answer1 == "2" {
 		Write("Wait You're supposed to take it... guess we don't have the choice... \n")
 		Write("You got bullied and lost consciousness")
 		player.health_point = 25
 	} else if answer1 == "69" || answer1 == "420" {
-		Write("That's a really cool number, you still \"die\" but you're cool :D")
+		Write("That's a really cool number, you still \"die\" but you're cool :D \n")
 		player.health_point = 100
 	} else {
-		Write("I like how you're trying to avoid dying but that's part of the game so please... die")
+		Write("I like how you're trying to avoid dying but that's part of the game so please... die \n")
 		player.health_point = 1
 	}
 
-	Write("You wake up, someone not dead, in a strange forest and find a \n\n")
+	Write("You wake up, somehow not dead, in a strange forest and find a \n\n")
 	Write("1 - GreatSword (slow but very powerful) \n2 - LongSword (fast and strong) \n3 - DualBlade (very quick but weak) \n")
 	answer2 := ""
 	fmt.Scanln(&answer2)
@@ -75,10 +75,16 @@ func main() {
 		player.speed = 5
 		player.damage = 5
 	}
-	Write("Now that you have a " + player.weapon + " equipped, you decide to go on an adventure throughout the forest!")
-
+	if player.name == "Kheir" || player.name == "Alan" || player.name == "Cyril" || player.name == "Ethan" {
+		Write("You just equipped the most powerful weapon in the universe, you'll get through this adventure with ease")
+		player.weapon = "Sexcalibur"
+		player.speed = 100
+		player.damage = 100
+	} else {
+		Write("Now that you have a " + player.weapon + " equipped, you decide to go on an adventure throughout the forest!")
+	}
 	player.level = 1
 	player.max_health_point = 100
 	player.money = 100
-	player.inventory = map[string]int{"First Aid": 3}
+	player.inventory = map[string]int{"First Aid": 5}
 }
